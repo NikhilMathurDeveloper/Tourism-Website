3503b37e4942cca56f17751473f8a733fa1db040
NikhilMathurDeveloper <nikhilmathur2004@gmail.com>
2024-11-17 18:26:58 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const parallax = document.getElementById("parallax");
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+})
+
+
+
+
+
+
